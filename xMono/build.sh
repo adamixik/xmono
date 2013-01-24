58162e20a0a5bb1c4c2aa6ed2d7f040355ad3ea0
ru.mysqld <ru.mysqld@89bbb25f-20b0-a86f-ba2d-70b5ff10c02d>
2013-01-24 17:56:58 +0000

    LEEENOOOX

--- xMono/build.sh
+++ xMono/build.sh
@@ -1,1 +1,1 @@
-g++ *.cpp -o ../Workfolder/xMono -I../Deps/mono/include -L../Deps/mono/lib -lmono-2.0 -lm -lrt -ldl -lpthread
+g++ *.cpp -o ../Workfolder/xMono.so -I../Deps/mono/include -L../Deps/mono/lib -lmono-2.0 -lm -lrt -ldl -lpthread -shared
---
+++ xMonoTest/build.sh
@@ -0,0 +1,1 @@
+g++ *.cpp -o ../Workfolder/xMonoTest ../Workfolder/xMono.so -DLINUX
